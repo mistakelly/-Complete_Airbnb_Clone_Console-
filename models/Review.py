dd43d4a1d5206b9edde48f5728b521ea981ccdd8
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-01 14:56:10 +0100

    Review

---
+++ models/Review.py
@@ -0,0 +1,14 @@
+from base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Public class attributes:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string - empty string
+    """
+
+    place_id: str = ''
+    user_id: str = ''
+    text: str = ''
