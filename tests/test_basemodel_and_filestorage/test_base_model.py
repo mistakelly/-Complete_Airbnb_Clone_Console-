0afc7468352d8d2a0ff5aaeecc85dd1ace67198d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-03 23:07:30 +0100

    Added teardown method

--- tests/test_basemodel_and_filestorage/test_base_model.py
+++ tests/test_basemodel_and_filestorage/test_base_model.py
@@ -9,6 +9,13 @@ from unittest.mock import MagicMock
 class TestBaseModel(unittest.TestCase):
 
     def setUp(self) -> None:
+        """
+        Set up resources required for each test method.
+
+        This method is called before each test method is executed. It initializes any objects or resources
+        that are needed for the tests to run. In this case, it creates an instance of the BaseModel class
+        and sets up mocking for the storage module to isolate the tests from external dependencies.
+        """
         self.model = BaseModel()
         # mocking to isolate save method of BaseModel with new and save_obj of filestorage
         self.mock_save_obj = MagicMock()
@@ -17,26 +24,32 @@ class TestBaseModel(unittest.TestCase):
         self.mock_new = MagicMock()
         models.storage.new = self.mock_new
 
+    def tearDown(self) -> None:
+        """
+        Teardown method to clean up resources after each test method.
+        """
+        del self.model
+
     #######################Custom Methods#####################
 
     def assert_hasattri(self, model, attribute) -> None:
         """
-            custom assert_hasattri Method.
-            checks for object presence and assert True or False.
+        custom assert_hasattri Method.
+        checks for object presence and assert True or False.
         """
         self.assertTrue(hasattr(model, attribute), f'{model} Attribute {attribute} does not exist')
 
     def assert_attribute_match(self, model, model_attribute) -> None:
         """
-            custom match object attribute after reloading to ensure attribute uniqueness.
+        custom match object attribute after reloading to ensure attribute uniqueness.
         """
         self.assertEqual(model, model_attribute, f"{model_attribute} don't match ")
 
     def assert_attribute_instance_types(self, model, attribute_dict: dict) -> None:
         """
-            custom assertIsinstance
-            parameter1: object_type
-            parameter2: dict
+        custom assertIsinstance
+        parameter1: object_type
+        parameter2: dict
         """
         for attribute_name, attribute_type in attribute_dict.items():
             self.assertIsInstance(model[attribute_name], attribute_type,
@@ -47,7 +60,7 @@ class TestBaseModel(unittest.TestCase):
         #######################test_methods Methods#################
     def test_module_and_BaseModel_doc_string(self) -> None:
         """
-            Responsible for checking for baseModel and base_model file docstring.
+        Responsible for checking for baseModel and base_model file docstring.
         """
         # baseModel file  and BaseModel class docstring
         self.assertIsNotNone(models.base_model.__doc__, f'Module {models.base_model} has no docstring')
@@ -55,7 +68,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_all_BaseModel_methods_doc_string(self) -> None:
         """
-            Responsible for checking all BaseModel Method docstring.
+        Responsible for checking all BaseModel Method docstring.
         """
         BaseModel_method = inspect.getmembers(self.model, inspect.ismethod)
         for name, method in BaseModel_method:
@@ -63,7 +76,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_init_method(self) -> None:
         """
-            Responsible for testing __init__ method
+        Responsible for testing __init__ method
         """
 
         # check if model has attributes
@@ -77,7 +90,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_custom_init_method(self) -> None:
         """
-            Responsible for testing the __init__ method when objects are reloaded.
+        Responsible for testing the __init__ method when objects are reloaded.
         """
         model_json = self.model.to_dict()
         model_dict = BaseModel(**model_json)  # custom reload_object
@@ -100,7 +113,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_str_method(self)-> None:
         """
-            Responsible for testing BaseModel __str__ Method.
+        Responsible for testing BaseModel __str__ Method.
         """
 
         # expected string format
@@ -115,7 +128,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_save_method(self) -> None:
         """
-            Responsible for testing BaseModel save Method.
+        Responsible for testing BaseModel save Method.
         """
         model_updated_at = self.model.updated_at
 
@@ -135,7 +148,7 @@ class TestBaseModel(unittest.TestCase):
 
     def test_to_dict_method(self) -> None:
         """
-            Responsible for testing BaseModel to_dict Method.
+        Responsible for testing BaseModel to_dict Method.
         """
         # Arrange: Convert object to dictionary format
         model_json = self.model.to_dict()
